cebe8f639f1be5e1f58bcce8aca7d0cf64bfba04
sark@bk.ru <Gir1i9n8x3>
2020-03-25 00:29:23 +0300

    Initial commit

---
+++ src/main/java/Menu.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Menu {
+    boolean exit;
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.printMenu();
+        menu.runMenu();
+    }
+
+public void printMenu(){
+    System.out.println("\n Let's play the game!");
+    System.out.println("Press 1 for Game1");
+    System.out.println("Press 2 for Game2");
+    System.out.println("Type EXIT to exit\n");
+}
+
+public void runMenu(){
+        while (!exit){
+            String choice = getInput();
+        }
+}
+
+public String getInput(){
+      String choice ="";
+      Scanner kb = new Scanner(System.in);
+      choice = kb.nextLine();
+
+      switch (choice){
+          case "1":
+              System.out.println("Playing game1");
+              break;
+          case "2":
+              System.out.println("Playing game2");
+              break;
+          case "EXIT":
+              System.out.println("now i am about to exit!");
+              exit = true;
+              break;
+          default:
+              System.out.println("unknown symbol.");
+      }
+
+      return choice;
+    }
+}
